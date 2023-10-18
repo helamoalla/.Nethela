31093f92398804123001b139982e00c2080e65a5
HelaMo <helamoalla91@gmail.com>
2023-10-18 09:33:49 +0100

    helamoalla

---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,28 @@
+﻿using AM.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure
+{
+    internal class AMContext: DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+            Initial Catalog=helamoalla;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
